0840a7824a89e60b92bdb938ba0874049a563234
Imperiumgamesites <128561414+Imperiumgamesites@users.noreply.github.com>
2023-03-22 19:57:56 -0500

    Update and rename main.js to alert.js

---
+++ alert.js
@@ -0,0 +1,19 @@
+<HEAD>
+<SCRIPT language="JavaScript">
+<!--hide
+
+var password;
+var pass1="sid";
+
+password=prompt('Please enter your password to view this page!',' ');
+
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    window.location="https://google.com/404";
+    }
+
+//-->
+</SCRIPT>
+</HEAD>
