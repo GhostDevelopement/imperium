fa4aa8185e22cfec99267e44f9beabc5b80bc0dd
Imperiumgamesites <128561414+Imperiumgamesites@users.noreply.github.com>
2023-03-22 20:02:23 -0500

    Update alert.js

--- alert.js
+++ alert.js
@@ -5,7 +5,7 @@
 var password;
 var pass1="sid";
 
-password=prompt('Please enter your password to view this page!',' ');
+alert password=prompt('Please enter your password to view this page!',' ');
 
 if (password==pass1)
   alert('Password Correct! Click OK to enter!');
